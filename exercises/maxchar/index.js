d2fcda8679af1a79d2f86deeb51dba24bc15d4ca
sheng yeong <chia.shengyeong@gmail.com>
2023-03-03 20:35:32 +0800

    max character

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const string_object = {};
+
+  let max = 0;
+  let maxChar = "";
+  for (let element of str) {
+    if (!string_object[element]) {
+      string_object[element] = 1;
+    } else {
+      string_object[element] ++;
+    }
+  }
+
+  for (let element in string_object) {
+    if (string_object[element] > max) {
+      max = string_object[element];
+      maxChar = element;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
