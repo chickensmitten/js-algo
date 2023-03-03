a2ef70d472b3a9ec2aa3cdfaa11958ab500c31c0
sheng yeong <chia.shengyeong@gmail.com>
2023-03-03 16:43:33 +0800

    truthy or falsy palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,17 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let reversed = str.split("").reverse().join("");
+  if (str === reversed) {
+    return true
+  } else {
+    return false
+  }
+}
+// The reverse() method reverses an array in place and returns the reference to the same array, 
+// the first array element now becoming the last, 
+// and the last array element becoming the first. 
+// In other words, elements order in the array will be turned towards the direction opposite to that previously stated.
 
 module.exports = palindrome;
