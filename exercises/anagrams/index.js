a77f5f34f066e9d5a2ab7e129fb4555737d92854
sheng yeong <chia.shengyeong@gmail.com>
2023-03-05 15:24:48 +0800

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,44 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  //  remove funny characters and lowercase them
+  const regexA = stringA.replace(/[^\w]/g, "").toLowerCase()
+  const regexB = stringB.replace(/[^\w]/g, "").toLowerCase()
+
+  let charMapA = maxChar(regexA)
+  let charMapB = maxChar(regexB)
+
+  let longCharMap
+  let shortCharMap
+
+  if (Object.keys(charMapA).length >= Object.keys(charMapB).length) {
+    longCharMap = charMapA 
+    shortCharMap = charMapB
+  } else {
+    longCharMap = charMapB
+    shortCharMap = charMapA
+  }
+  for (const char in longCharMap) {
+    if (shortCharMap === undefined || (longCharMap[char] != shortCharMap[char])) {
+      return false
+    }
+  }
+  return true
+}
+
+function maxChar(str) {
+  const charMap = {};
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  return charMap;
+}
 
 module.exports = anagrams;
